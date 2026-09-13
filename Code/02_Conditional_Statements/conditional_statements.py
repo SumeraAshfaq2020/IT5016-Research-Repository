63d080c861d2a37e939f026859d21dfa170f6586
sumera <20267419@mywhitecliffe.com>
2026-09-13 20:39:08 +1200

    Update conditional_statements.py

--- Code/02_Conditional_Statements/conditional_statements.py
+++ Code/02_Conditional_Statements/conditional_statements.py
@@ -1,1 +1,13 @@
+# Conditional statements practice
+# This program uses if, elif and else
 
+# Taking age as input
+age = int(input("Enter your age: "))
+
+# Checking the age using conditions
+if age < 13:
+    print("You are a child.")
+elif age < 18:
+    print("You are a teenager.")
+else:
+    print("You are an adult.")
