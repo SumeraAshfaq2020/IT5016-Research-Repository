8ff1001f40c4600360f35633cf0d4b78881f097e
sumera <20267419@mywhitecliffe.com>
2026-09-13 20:46:06 +1200

    Implement add_numbers function with user input
    Added a function to add two numbers and demonstrate local and global variables.

--- Code/03_Functions_and_Variables/funtions_variables.py
+++ Code/03_Functions_and_Variables/funtions_variables.py
@@ -1,1 +1,28 @@
+# Functions with parameters and return values
+# This program also demonstrates local and global variables
 
+# Global variable
+school_name = "Whitecliffe College"
+
+
+# Function with parameters
+def add_numbers(number1, number2):
+    # Local variable
+    total = number1 + number2
+
+    # Return the result to the function call
+    return total
+
+
+# Getting values from the user
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+# Calling the function and passing arguments
+answer = add_numbers(first_number, second_number)
+
+# Displaying the returned result
+print("The total is:", answer)
+
+# Using the global variable
+print("College:", school_name)
